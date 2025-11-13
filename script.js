14a2938515284fe7bb31a5901cf07dcdc6276f24
TangsangR <160494956+TangsangR@users.noreply.github.com>
2025-11-13 15:17:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let display = document.getElementById('display');
+let historyBox = document.getElementById('historyBox');
+let history = [];
+ //add value to display
+function append(e) {
+    if (display.innerText === '0' || display.innerText === 'Error') //if display is 0 or Error, replace it
+    {
+        display.innerText = e;
+    } 
+    else {
+        display.innerText += e; //if no 0 or error, add it
+    
+}}
+// Clear the display
+function clearDisplay() {
+    display.innerText = '0';
+}
+//del last character
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1) || '0';
+}
+
+    function calculate() {
+    try {
+        let expression = display.innerText;
+        let result = eval(expression);
+        history.push(display.innerText + ' = ' + result); //add to history when answer is calculated
+        display.innerText = result;
+        updateHistory();
+}
+    catch (e) {
+        display.innerText = 'Error'; //display error when wrong
+      }
+    }
+
+ //update history box
+    function updateHistory() 
+    {
+      historyBox.innerHTML = history.map(item => `<div>${item}</div>`).join('');
+    }
+    function toggleHistory() 
+    {
+      if (historyBox.style.display === 'none' || historyBox.style.display === '') {
+        historyBox.style.display = 'block';
+      } else {
+        historyBox.style.display = 'none';
+      }
+    }
